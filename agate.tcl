c6e5893b52d75f1212747a8596a4e186b99a8a99
Tony Aldridge <zaragopha@hotmail.com>
2013-11-09 12:54:53 +0000

    Started on Routing

--- agate.tcl
+++ agate.tcl
@@ -12,7 +12,14 @@ proc ::agate::application {} {
     variable last_id
 
     set id "agate[incr last_id]"
-    dict set app_list $id {}
+    dict set app_list $id {
+        routes [dict_create \ 
+            get [list] \
+            post [list] \
+            put [list] \
+            delete [list] \
+        ]
+    }
     return $id
 }
 
@@ -24,15 +31,19 @@ proc ::agate::run {app {cleanup 1} } {
 }
 
 proc ::agate::get {app, path, method} {
+    ::agate::set_route $app get $path $method
 }
 
 proc ::agate::post {app, path, method} {
+    ::agate::set_route $app post $path $method
 }
 
 proc ::agate::put {app, path, method} {
+    ::agate::set_route $app put $path $method
 }
 
 proc ::agate::delete {app, path, method} {
+    ::agate::set_route $app delete $path $method
 }
 
 proc ::agate::cleanup {app} {
@@ -41,4 +52,9 @@ proc ::agate::cleanup {app} {
     puts "CLEAN UP"
     puts $last_id
     dict unset app_list $app
+}
+
+proc ::agate::set_route {app, type, path, method} {
+    variable app_list
+    set paths [dict get app_list $app routes $type]
 }
