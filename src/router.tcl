bd8ac452851871ad4d712f0419c96575d2f0e5cc
Tony Aldridge <zaragopha@hotmail.com>
2013-11-16 13:18:49 +0000

    Migrated router to OO style

--- src/router.tcl
+++ src/router.tcl
@@ -10,46 +10,35 @@ itcl::class ::agate::router::Router {
     }
 
     method setRoute {type path method} {
+        set paths [dict get $routes $type]
+        lappend paths [list $path $method]
+        dict set routes $type $paths
     }
 
     method getRoutes {type} {
+        return [dict get $routes $type]
     }
 
+    # Check against a given method type whether any URLs match it
+    # @param appVar The Agate dictionary
+    # @param type The method type to match
+    # @param url The input url
+    #
+    # @return {callback, captured variables} 
+    # TODO: This should also handle python-style named params
     method matchRoute {type url} {
-    }
-}
-
-proc ::agate::router::setRoute {appVar type path method} {
-    upvar $appVar app
-    set paths [dict get $app routes $type]
-    lappend paths [list $path $method]
-    dict set app routes $type $paths 
-}
-
-proc ::agate::router::getRoutes {appVar type} {
-    upvar $appVar app
-    return [dict get $app routes $type]
-}
-
-# Check against a given method type whether any URLs match it
-# @param appVar The Agate dictionary
-# @param type The method type to match
-# @param url The input url
-#
-# @return {callback, captured variables} 
-# TODO: This should also handle python-style named params
-proc ::agate::router::matchRoute {app type url} {
-    set paths [dict get $app routes $type]
-    foreach {path} $paths {
-        set result [regexp -inline -- [lindex $path 0] $url]
-        if {[llength $result] > 0} {
-            # If we have captured variables, we need to return them
-            if {[llength $result] > 1} {
-                set params [lrange $result 1 end]
-            } else {
-                set params {}
+        set paths [dict get $routes $type]
+        foreach {path} $paths {
+            set result [regexp -inline -- [lindex $path 0] $url]
+            if {[llength $result] > 0} {
+                # If we have captured variables, we need to return them
+                if {[llength $result] > 1} {
+                    set params [lrange $result 1 end]
+                } else {
+                    set params {}
+                }
+                return [list [lindex $path 1] $params]
             }
-            return [list [lindex $path 1] $params]
         }
     }
 }
